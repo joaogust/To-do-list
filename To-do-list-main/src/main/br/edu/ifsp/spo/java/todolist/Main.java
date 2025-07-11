8d775d963632ddaad162dbff561ed76b3dbcbcc5
Andrey Gustavo <andreygustavo267@gmail.com>
2025-07-10 21:31:36 -0300

    Conexão com Banco de Dados e desenvolvimento de algumas funções, construtores, getters e setters

--- To-do-list-main/src/main/br/edu/ifsp/spo/java/todolist/Main.java
+++ To-do-list-main/src/main/br/edu/ifsp/spo/java/todolist/Main.java
@@ -4,7 +4,6 @@ import main.br.edu.ifsp.spo.java.todolist.connection.ConnectionDB;
 import main.br.edu.ifsp.spo.java.todolist.connection.TaskDB;
 import main.br.edu.ifsp.spo.java.todolist.core.Task;
 
-import java.awt.*;
 import java.util.List;
 
 public class Main {
@@ -15,12 +14,31 @@ public class Main {
 
         Task newTask = new Task("Estudar JDBC");
         TaskDB taskDB = new TaskDB();
+        taskDB.insert(newTask);
 
+        // Esses são testes pra ver se o SELECT funciona, depois a gente tira
+        List<Task> allTasks = taskDB.findAllTasks();
 
-
-        List<Task> allTasks = taskDB.findTasks();
-        // Esse é um teste pra ver se o SELECT funciona, depois a gente tira
         for (Task task : allTasks) {
+            System.out.println("Id: " + task.getIdTask());
+            System.out.println("Descrição: " + task.getText());
+            System.out.println("Concluída: " + task.isCompleted());
+            System.out.println("Data de alteração: " + task.getChangeDate());
+            System.out.println("--------");
+        }
+
+        List<Task> pendingTasks = taskDB.findPending();
+        for (Task task : pendingTasks) {
+            System.out.println("Id: " + task.getIdTask());
+            System.out.println("Descrição: " + task.getText());
+            System.out.println("Concluída: " + task.isCompleted());
+            System.out.println("Data de alteração: " + task.getChangeDate());
+            System.out.println("--------");
+        }
+
+        List<Task> findCompleted = taskDB.findCompleted(); // Não deve exibir nada por enquanto, pois nenhuma task está completa
+        for (Task task : findCompleted) {
+            System.out.println("Id: " + task.getIdTask());
             System.out.println("Descrição: " + task.getText());
             System.out.println("Concluída: " + task.isCompleted());
             System.out.println("Data de alteração: " + task.getChangeDate());
--- To-do-list-main/src/main/br/edu/ifsp/spo/java/todolist/core/Task.java
+++ To-do-list-main/src/main/br/edu/ifsp/spo/java/todolist/core/Task.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.time.LocalDateTime;
 
 public class Task {
+
+    private int idTask; // id da tarefa
     private String text; // Descrição da tarefa
     private boolean completed; // Tarefa concluída ou não
     private LocalDateTime changeDate; // Data de alteração
@@ -13,10 +15,25 @@ public class Task {
     // private SimpleDateFormat completionDate; // Data de conclusão
     // private Status status; // Não iniciada | Em andamento | Concluído
 
-    public Task(String text) {  // Construtor
+    public Task(String text) {  // Construtor para criar tareas no sstema
         this.text = text;  // Texto para descrever a tarefa
         this.completed = false; // Tarefa começa não concluída
         this.changeDate = LocalDateTime.now(); // Função para definir data e hora de criação
+    }
+
+    public Task(int idTask, String text, boolean completed, LocalDateTime changeDate) { // Construtor para o bdd
+        this.idTask = idTask;
+        this.text = text;
+        this.completed = completed;
+        this.changeDate = changeDate;
+    }
+
+    public int getIdTask() {
+        return idTask;
+    }
+
+    public void setIdTask(int idTask) {
+        this.idTask = idTask;
     }
 
     public String getText() {
